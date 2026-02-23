754f1449d51247e0a6ad5a6df32f1063bce245d7
vxhuy <vxhuy17694@gmail.com>
2026-02-23 14:09:52 +0700

    DAMM update: v1.0.3
    Changes included:
    - update package
    - update package
    - released Damm 1.0.1
    - released Damm 1.0.1
    - released Damm 1.0.0
    - update damm mainnet
    - update package
    - add docs and update logic
    - update logic
    - update simulate swap
    - fix add liquidity
    - update functions
    - update damm
    - update package
    - update lock position
    - fix create position
    - fix swap calculate rates
    - fix calculate swap
    - update pool response
    - remove logs
    
    Package: @ferra-labs/damm
    Version: v1.0.3
    Tag: @ferra-labs/damm@v1.0.3
    Repository: Ferra-Labs/ferra-sdks
    
    ---
    Released from phoenix0x02/ferra-sdks
    Build Date: 2026-02-23 14:09:52

--- packages/damm/src/config/mainnet.ts
+++ packages/damm/src/config/mainnet.ts
@@ -3,9 +3,9 @@ import { checkValidSuiAddress } from '../utils'
 
 const SDKConfig = {
   dammConfig: {
-    pools_id: '0x16b4ec2ead4377b11fe56479b19809885198d0cb3422d2e1048a160c8f9ba1e5',
-    global_config_id: '0x3bf945ddf84971e57497ee5e733d77984f540db6b07b6fdb00568ce2d4b0f517',
-    global_rewarder_vault_id: '0x8c4946886cd31a6d213b235e245ba2a8e39d2877993ee493f6a4ede060f68d12'
+    pools_id: '0xf7774e82131b6c581987000046826d1b902c7acba2887d26cc2bccfeff1ace01',
+    global_config_id: '0x872637215dbee83854692ac83237a34eaae8ef784d43548fc80ac6c7b5c8fdd1',
+    global_rewarder_vault_id: '0xafe43ac8ba302f77f9d9ba46ec1a1d43e748a474b7616a99242ee49ca31adfd5'
   }
 }
 
@@ -16,13 +16,13 @@ export const dammMainnet: SdkOptions = {
     address: '0x0000000000000000000000000000000000000000000000000000000000000000',
   },
   damm_pool: {
-    package_id: '0x66fb6a132c415278c32ab52ecdc2bd73b08b649e396841f48f6f9cccd01b6bbb',
-    published_at: '0x5810a6545d6046d98eee8867ed1106ab5fc472f36feda8acdd3f0a331968cef8',
+    package_id: '0x9c81b37498c1a49e18993b358fdf715154e36663f99147ef98b7a0c8bfbc9d4e',
+    published_at: '0x9c81b37498c1a49e18993b358fdf715154e36663f99147ef98b7a0c8bfbc9d4e',
     config: SDKConfig.dammConfig,
   },
   integrate: {
-    package_id: '0x8df3a90c236e6555bc5edc9c11165dbddf456814f8c670d03e729f92367ce473',
-    published_at: '0x8df3a90c236e6555bc5edc9c11165dbddf456814f8c670d03e729f92367ce473',
+    package_id: '0x37e8a1abf987431fa2c732808a45d3f4d307cd5ced734b920244540fd8be3a3e',
+    published_at: '0x37e8a1abf987431fa2c732808a45d3f4d307cd5ced734b920244540fd8be3a3e',
   },
   swapCountUrl: 'https://api.ferra.xyz/damm/swap/pools'
 }
